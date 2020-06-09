9c4e1821047eb96090f67cb866c0cfdc852073a7
Liam Toney <ltoney@usgs.gov>
2020-06-09 13:10:23 -0600

    Implement careful rotation

--- lsforce/lsdata.py
+++ lsforce/lsdata.py
@@ -1,7 +1,5 @@
 from obspy.geodetics import gps2dist_azimuth
-from obspy.signal.rotate import rotate2zne
-from obspy import Stream, Trace
-import urllib
+from obspy.clients.fdsn import Client
 import numpy as np
 
 KM_PER_M = 1 / 1000  # [km/m]
@@ -11,8 +9,8 @@ class LSData:
     """Class for force inversion data (essentially a souped-up Stream).
 
     Attributes:
-        st_orig:
-        st_proc:
+        st_orig: Original input Stream `st`
+        st_proc: Stream rotated into RTZ w.r.t. `source_lat`, `source_lon`
         source_lat:
         source_lon:
     """
@@ -48,118 +46,79 @@ class LSData:
             tr.stats.azimuth = az  # [deg]
             tr.stats.back_azimuth = baz  # [deg]
 
-        # Rotate into Z–R–T (on a station-by-station basis!)
-        for station in np.unique([tr.stats.station for tr in self.st_proc]):
-            self.st_proc.select(station=station).rotate('NE->RT')
+        # Rotate into RTZ
+        self.st_proc = _rotate_to_rtz(self.st_proc)
 
         self.st_proc.sort(keys=['distance', 'channel'])
 
 
-def rotate(st, back_azimuth=None):
-    """
-    rotate all components of st that can be rotated, to radial and transverse
+def _rotate_to_rtz(st):
+    """Rotates all components of a Stream into radial–transverse–vertical.
+
+    This function first rotates non-standard horizontals into EN, then rotates into
+    RTZ. It also checks that input components labeled as east, north, and vertical
+    (e.g., BHE, BHN, BHZ, etc.) have the correct orientation.
 
     Args:
-        st: obspy stream object to rotate
-        back_azimuth (list): Not required if backaz already attached to st stats, list
-            of backazimuths corresponding to st
+        st: ObsPy Stream
+
+    Returns:
+        Rotated Stream
     """
 
-    # implant back_azimuth in st's
-    if back_azimuth:
-        for i, trace in enumerate(st):
-            trace.stats.back_azimuth = back_azimuth[i]
-            st[i] = trace
-    else:
-        try:
-            st[0].stats.back_azimuth
-        except:
-            print('need to attach back_azimuth')
-            return
-    # get list of station location code pairs present
-    staloc = list(
-        set([trace.stats.station + '.' + trace.stats.location for trace in st])
-    )
-    st_rotated = Stream(traces=Trace())  # initialize, pop this one off later
-    for station in staloc:
-        # get all components with that station name
-        loc = station.split('.')[1]
-        if loc == '':
-            loc = None
-        st_temp = st.select(
-            station=station.split('.')[0], location=loc
-        ).copy()  # [trace for trace in st if station in trace.stat.station]
-        if len(st_temp) == 3:  # if len 3, put z down, rotate horizontals
-            try:
-                z = st_temp.select(component='Z').copy()
-                st_rotated = st_rotated + z.copy()
-                chans = [trace.stats.channel for trace in st_temp]
-                # if not BHN BHE
-                if 'H1' in str(chans) and 'H2' in str(chans):
-                    try:
-                        for k, trace in enumerate(st_temp):
-                            if trace.stats.location == '':
-                                loc = '--'
-                            else:
-                                loc = trace.stats.location
-                            url = (
-                                'http://service.iris.edu/fdsnws/station/1/query?net=%s&sta=%s&loc=%s&cha=%s&level=channel&format=text&includecomments=true&nodata=404'
-                                % (
-                                    trace.stats.network,
-                                    trace.stats.station,
-                                    loc,
-                                    trace.stats.channel,
-                                )
-                            )
-                            temp = urllib.request.urlopen(url)
-                            file1 = temp.read()
-                            lines = [line.split('|') for line in file1.split('\n')[1:]]
-                            trace.stats.cmpaz = float(lines[0][8])
-                            st_temp[k] = trace
+    st_rot = st.copy()  # Work on a copy of the data
 
-                        z1, n1, e1 = rotate2zne(
-                            z[0].data,
-                            0,
-                            -90,
-                            st_temp.select(component='1')[0].data,
-                            st_temp.select(component='1')[0].stats.cmpaz,
-                            0,
-                            st_temp.select(component='2')[0].data,
-                            st_temp.select(component='2')[0].stats.cmpaz,
-                            0,
-                        )
-                        st_temp.select(component='1')[0].data = n1
-                        st_temp.select(component='1')[0].stats.channel = 'BHN'
-                        st_temp.select(component='2')[0].data = e1
-                        st_temp.select(component='2')[0].stats.channel = 'BHE'
-                    except:
-                        print(
-                            'couldnt get cmpaz orientation from IRIS, rotation failed'
-                        )
-                        continue
-                st_h = (
-                    st_temp.select(component='N').copy()
-                    + st_temp.select(component='E').copy()
-                )
-                st_h.rotate('NE->RT')
-                st_rotated = st_rotated + st_h.copy()
-            except:
-                print('error in rotating for ' + station + ' -skipping')
-        elif len(st_temp) == 1:  # if len 1, put z down, continue
-            z = st_temp.select(component='Z')
-            st_rotated = st_rotated + z.copy()
-        elif len(st_temp) == 2:  # if len 2, probably horizontal components
-            try:
-                st_h = (
-                    st_temp.select(component='N').copy()
-                    + st_temp.select(component='E').copy()
-                )
-                st_h.rotate('NE->RT')
-                st_rotated = st_rotated + st_h.copy()
-            except:
-                print(('weird number of components for ' + station + ' -skipping'))
+    # Assemble info for inventory gather
+    networks = np.unique([tr.stats.network for tr in st_rot])
+    stations = np.unique([tr.stats.station for tr in st_rot])
+    channels = np.unique([tr.stats.channel for tr in st_rot])
+
+    # Grab inventory for orientation info (relying only on IRIS FDSN here!)
+    client = Client('IRIS')
+    inv = client.get_stations(network=','.join(networks), station=','.join(stations),
+                              channel=','.join(channels),
+                              starttime=st_rot[0].stats.starttime,
+                              endtime=st_rot[0].stats.endtime, level='channel')
+
+    # Rotate on a station-by-station basis
+    for station in stations:
+
+        st_sta = st_rot.select(station=station)  # Just select Traces for this station
+        components = [tr.stats.channel[-1] for tr in st_sta]
+        components.sort()  # Increasing numerical, then alphabetical order
+
+        # ENZ case (or subset of ENZ)
+        if components in (
+                ['E', 'N', 'Z'],
+                ['E', 'N'],
+                ['E', 'Z'],
+                ['N', 'Z'],
+                ['E'],
+                ['N'],
+                ['Z']
+        ):
+            # Just check for expected orientation, don't do any rotation
+            for component in components:
+
+                id = st_sta.select(component=component)[0].id  # Only expecting one Trace here!
+                msg = f'Unexpected orientation for {id}'
+                orient = inv.get_orientation(id)
+                if component == 'E':
+                    assert orient['azimuth'] == 90.0 and orient['dip'] == 0.0, msg
+                elif component == 'N':
+                    assert orient['azimuth'] == 0.0 and orient['dip'] == 0.0, msg
+                else:  # component == 'Z'
+                    assert orient['azimuth'] == 0.0 and orient['dip'] == -90.0, msg
+
+        # 12Z and 123 cases
+        elif components in (['1', '2', 'Z'], ['1', '2', '3']):
+            st_sta.rotate('->ZNE', inventory=inv)  # Rotate to ENZ (in-place modification!)
+
+        # Error out since we don't know how to handle this (yet)!
         else:
-            print(('weird number of components for ' + station + ' -skipping'))
-    st_rotated.pop(0)  # pop off the placeholder
+            raise ValueError(f'Unable to rotate station {station}')
 
-    return st_rotated
+        # Now the data are ostensibly in correct ENZ orientation - do the actual rotation to RTZ
+        st_sta.rotate('NE->RT')
+
+    return st_rot
