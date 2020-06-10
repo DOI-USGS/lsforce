f970b7365be19d176c92aca273157ffafda53f9a
Liam Toney <ltoney@usgs.gov>
2020-06-10 09:32:32 -0600

    Add install.sh
    Taken from
    https://github.com/usgs/groundfailure/blob/e4e34f2ff99fe163d98fa3bdbb7dbd2a367e7f61/install.sh

---
+++ install.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+
+unamestr=`uname`
+if [ "$unamestr" == 'Linux' ]; then
+    prof=~/.bashrc
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    matplotlibdir=~/.config/matplotlib
+elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
+    prof=~/.bash_profile
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    matplotlibdir=~/.matplotlib
+else
+    echo "Unsupported environment. Exiting."
+    exit
+fi
+
+source $prof
+
+# Name of new environment (must also change this in .yml files)
+VENV=gf
+# Python version
+py_ver=3.6
+
+# Set to 1 if you are a developer and want ipython etc. installed
+developer=0
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+    # if mkdir fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to create matplotlib configuration file. Exiting."
+        exit 1
+    fi
+fi
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
+fi
+
+
+# Is conda installed?
+conda --version
+if [ $? -ne 0 ]; then
+    echo "No conda detected, installing miniconda..."
+
+    curl -L $mini_conda_url -o miniconda.sh;
+
+    # if curl fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to create download miniconda installer shell script. Exiting."
+        exit 1
+    fi
+
+    echo "Install directory: $HOME/miniconda"
+
+    bash miniconda.sh -f -b -p $HOME/miniconda
+
+    # if miniconda.sh fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to run miniconda installer shell script. Exiting."
+        exit 1
+    fi
+
+    . $HOME/miniconda/etc/profile.d/conda.sh
+else
+    echo "conda detected, installing $VENV environment..."
+fi
+
+# echo "PATH:"
+# echo $PATH
+# echo ""
+
+# add source command to profile file if it isn't already there
+grep "/etc/profile.d/conda.sh" $prof
+if [ $? -ne 0 ]; then
+    echo ". $_CONDA_ROOT/etc/profile.d/conda.sh" >> $prof
+fi
+
+#env_file=environment.yml
+
+# Start in conda base environment
+echo "Activate base virtual environment"
+conda activate base
+
+# Remove existing shakemap environment if it exists
+conda remove -y -n $VENV --all
+
+dev_list=(
+    "ipython"
+    "spyder"
+    "sphinx"
+    "sphinx-argparse"
+)
+
+# Package list:
+package_list=(
+      "python=$py_ver"
+      "basemap"
+      "basemap-data-hires"
+      "configobj"
+      "descartes"
+      "fiona"
+      "folium"
+      "gdal"
+      "impactutils"
+      "libcomcat"
+      "mapio"
+      "matplotlib-base"
+      "numpy"
+      "pytables"
+      "pytest"
+      "pytest-cov"
+      "pytest-faulthandler"
+      "rasterio"
+      "scikit-image"
+      "scipy"
+      "simplekml"
+)
+
+if [ $developer == 1 ]; then
+    package_list=( "${package_list[@]}" "${dev_list[@]}" )
+    echo ${package_list[*]}
+fi
+
+# Create a conda virtual environment
+echo "Creating the $VENV virtual environment"
+# conda env create -f $env_file --force
+conda create -y -n $VENV -c defaults -c conda-forge \
+      --strict-channel-priority ${package_list[*]}
+
+# Bail out at this point if the conda create command fails.
+# Clean up zip files we've downloaded
+if [ $? -ne 0 ]; then
+    echo "Failed to create conda environment.  Resolve any conflicts, then try again."
+    exit
+fi
+
+
+# Activate the new environment
+echo "Activating the $VENV virtual environment"
+conda activate $VENV
+
+# if conda activate fails, bow out gracefully
+if [ $? -ne 0 ];then
+    echo "Failed to activate ${VENV} conda environment. Exiting."
+    exit 1
+fi
+
+# upgrade pip, mostly so pip doesn't complain about not being new...
+pip install --upgrade pip
+
+# if pip upgrade fails, complain but try to keep going
+if [ $? -ne 0 ];then
+    echo "Failed to upgrade pip, trying to continue..."
+    exit 1
+fi
+
+# This package
+echo "Installing $VENV"
+pip install -e .
+
+# if pip install fails, bow out gracefully
+if [ $? -ne 0 ];then
+    echo "Failed to pip install this package. Exiting."
+    exit 1
+fi
+
+# Tell the user they have to activate this environment
+echo "Type 'conda activate $VENV' to use this new virtual environment."
