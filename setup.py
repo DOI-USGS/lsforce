1832d517bce6a6b384bd008145847343ee7c6182
Liam Toney <ltoney@usgs.gov>
2020-06-23 09:44:48 -0600

    Add script for producing CPS model96 file from IRIS model

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='lsforce', packages=find_packages(), install_requires=[])
+setup(
+    name='lsforce',
+    packages=find_packages(),
+    scripts=['bin/produce_ak135f'],
+    install_requires=[],
+)
