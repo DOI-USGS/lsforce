9a58030d2ea169ea66264f0524768dc85941c78a
Liam Toney <ltoney@usgs.gov>
2020-07-21 16:48:12 -0600

    Remove next_pow_2 GF length constraint from CPS since not present in Syngine

--- lsforce/lsforce.py
+++ lsforce/lsforce.py
@@ -198,9 +198,6 @@ class LSForce:
 
                 # Write the "dist" file
                 gf_length_samples = int(self.gf_duration * self.data_sampling_rate)
-                if self.domain == 'frequency':
-                    # Use a fast length
-                    gf_length_samples = next_pow_2(gf_length_samples)
                 with open('dist', 'w') as f:
                     for sta in stations_to_calculate:
                         dist = self.data.st_proc.select(station=sta)[0].stats.distance
