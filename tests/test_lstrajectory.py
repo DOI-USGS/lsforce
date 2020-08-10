a15dc35a58b308d1847ee0c7312b396a74c1b989
Liam Toney <ltoney@usgs.gov>
2020-08-10 17:55:58 -0600

    Add very general test for LSTrajectory

---
+++ tests/test_lstrajectory.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+
+import pytest
+
+from lsforce import LSTrajectory
+
+# Set kwargs for all pytest-mpl tests
+PYTEST_MPL_KWARGS = dict(style='default', savefig_kwargs=dict(bbox_inches='tight'))
+
+# Get location of test data
+script_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(script_dir, 'data')
+
+# Grab LSForce object from test data dir to use for LSTrajectory creation
+with open(os.path.join(data_dir, 'lsforce.pkl'), 'rb') as f:
+    lsforce = pickle.load(f)
+
+
+@pytest.mark.mpl_image_compare(**PYTEST_MPL_KWARGS)
+def test_lstrajectory_calculation_and_plots():
+
+    print('Testing LSTrajectory calculation and plots...')
+
+    # Create LSTrajectory object (which also calculates trajectory)
+    trajectory = LSTrajectory(
+        lsforce, target_length=6, duration=200, detrend_velocity=200
+    )
+
+    # Return map-view figure
+    return trajectory.plot_trajectory()
