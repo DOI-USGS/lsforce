27197dc1d3a0f6ca660c9d6c16d488abad8b611b
kallstadt-usgs <kallstadt@usgs.gov>
2020-10-06 18:45:49 -0600

    another black formatting error

--- lsforce/__init__.py
+++ lsforce/__init__.py
@@ -1,7 +1,6 @@
 from ._version import get_versions
 from .lsdata import LSData
-from .lsforce import LSForce
-from .lsforce import readrun
+from .lsforce import LSForce, readrun
 from .lstrajectory import LSTrajectory
 
 __version__ = get_versions()['version']
