479b28ae2e95fca5289dd5f7e1c24219b16e0e0d
Liam Toney <ltoney@usgs.gov>
2022-09-07 15:59:05 -0800

    Another doc update

--- doc/_build/html/_static/documentation_options.js
+++ doc/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.0.1+88.g9bc8612.dirty',
+    VERSION: 'v1.0.1+89.g524d4c9.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
